11b5e12517cbf8ba8edccb421beea3a9ae870b45
CorneaAdrianEmilian <88203225+CorneaAdrianEmilian@users.noreply.github.com>
2025-09-18 14:08:56 +0300

    initial commit

---
+++ components/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import Profile from "./Profile"
+import { profile } from "../data"
+
+
+export default function App()
+{
+return(
+  <main>
+    <Header/>
+    <Profile picture={profile.picture} greetings={profile.greetings} description ={profile.description}/>
+  </main>
+)
+}
