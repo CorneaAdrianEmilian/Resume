f7ba3a5005fed9b77c1b6ea35ab8fc1cf2a28552
CorneaAdrianEmilian <emilian.cornea@yahoo.com>
2025-09-18 14:12:04 +0300

    test

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header()
+{
+
+  return(
+    <header>
+        <span>About Me</span>
+        <span>Education</span>
+        <span>Job Experience</span>
+        <span>Skills</span>
+        <span>Projects</span>
+    </header>
+  )
+}
---
+++ components/Profile.jsx
@@ -0,0 +1,16 @@
+export default function Profile(props)
+{
+return(
+<section className="profile_container">
+
+<img className="profile_picture" src={props.picture} alt="profile pic" />
+
+<div className="profile_text_container">
+<p className="profile_greetings">{props.greetings}</p>
+<p className="profile_description">{props.description}</p>
+</div>
+
+</section>
+)
+
+}
