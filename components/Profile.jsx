c660752d4093f85f636741a3ccdb1f1ff8bd00ec
CorneaAdrianEmilian <emilian.cornea@yahoo.com>
2025-09-18 14:30:45 +0300

    testing merging

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function Profile(props)
 {
 return(
@@ -14,21 +13,4 @@ return(
 </section>
 )
 
-=======
-export default function Profile(props)
-{
-return(
-<section className="profile_container">
-
-<img className="profile_picture" src={props.picture} alt="profile pic" />
-
-<div className="profile_text_container">
-<p className="profile_greetings">{props.greetings}</p>
-<p className="profile_description">{props.description}</p>
-</div>
-
-</section>
-)
-
->>>>>>> 11b5e12517cbf8ba8edccb421beea3a9ae870b45
 }
--- index.jsx
+++ index.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import ReactDOM from "react-dom/client"
 import App from "./components/App"
 
-=======
-import ReactDOM from "react-dom/client"
-import App from "./components/App"
-
->>>>>>> 11b5e12517cbf8ba8edccb421beea3a9ae870b45
 ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
