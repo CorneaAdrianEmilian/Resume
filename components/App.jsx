18d16cba35e4f0864fdd0f2c1f54bf47abd7fa62
CorneaAdrianEmilian <emilian.cornea@yahoo.com>
2025-09-18 14:27:16 +0300

    link branch

--- components/App.jsx
+++ components/App.jsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import Header from "./Header"
-import Profile from "./Profile"
-import { profile } from "../data"
-
-
-export default function App()
-{
-return(
-  <main>
-    <Header/>
-    <Profile picture={profile.picture} greetings={profile.greetings} description ={profile.description}/>
-  </main>
-  /* test3*/ 
-)
-=======
 import Header from "./Header"
 import Profile from "./Profile"
 import { profile } from "../data"
@@ -27,5 +11,4 @@ return(
     <Profile picture={profile.picture} greetings={profile.greetings} description ={profile.description}/>
   </main>
 )
->>>>>>> 11b5e12517cbf8ba8edccb421beea3a9ae870b45
 }
